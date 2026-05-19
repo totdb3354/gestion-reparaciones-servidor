dec2f4365175ced97558fcfcff272139bec53287
Imad <totdb3354@gmail.com>
2026-05-19 12:08:28 +0200

    feat: campo modelo en Telefono model

--- src/main/java/com/reparaciones/servidor/model/Telefono.java
+++ src/main/java/com/reparaciones/servidor/model/Telefono.java
@@ -2,12 +2,17 @@ package com.reparaciones.servidor.model;
 
 public class Telefono {
     private String imei;
+    private String modelo;
 
     public Telefono() {}
 
-    public Telefono(String imei) {
-        this.imei = imei;
+    public Telefono(String imei) { this.imei = imei; }
+
+    public Telefono(String imei, String modelo) {
+        this.imei   = imei;
+        this.modelo = modelo;
     }
 
-    public String getImei() { return imei; }
+    public String getImei()   { return imei; }
+    public String getModelo() { return modelo; }
 }
